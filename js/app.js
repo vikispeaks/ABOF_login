652289aa3de82c5aeb828b1c962f4f4e6c2706e1
Vignesh Rajendran <vign.ran@gmail.com>
2016-10-05 19:07:39 +0530

    Base validation

--- js/app.js
+++ js/app.js
@@ -7,7 +7,25 @@ var App = {
 	},
 
 	/*Validate Form elements*/
-	validateForm: function(e, itemIndex) {
+	validateForm: function() {
+
+		var firstName = document.getElementById("first-name");
+		var lastName = document.getElementById("last-name");
+		var email = document.getElementById("user-email");
+		var password = document.getElementById("user-pasword");
+		var mobile = document.getElementById("user-mobile");
+		var errorMsg = document.createElement('span');
+		// Regular Expressions
+		var regExp = {
+				email : /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+				password : /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+				mobile : /^[789]\d{9}$/
+		};
+
+		if(!(email.value.match(regExp.email))) {
+					email.parentNode.appendChild(errorMsg);
+		}
+
 
 	}
 };
